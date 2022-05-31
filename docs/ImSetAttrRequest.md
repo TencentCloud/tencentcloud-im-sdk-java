84edb5fd7db8186b9fd82a5e6009be795685234e
shifpeng <shifpeng@163.com>
2022-05-31 10:59:41 +0800

    [Mod] update im_set_attr interface request params

--- docs/ImSetAttrRequest.md
+++ docs/ImSetAttrRequest.md
@@ -7,8 +7,7 @@
 
 | Name | Type | Description | Notes |
 |------------ | ------------- | ------------- | -------------|
-|**toAccount** | **String** | 目标用户帐号列表 |  |
-|**attrs** | **Object** | 属性集合。每个属性是一个键值对，键为属性名，值为该用户对应的属性值。用户属性值不能超过50字节 |  |
+|**userAttrs** | [**List&lt;ImGetAttrResponseAllOfUserAttrs&gt;**](ImGetAttrResponseAllOfUserAttrs.md) |  |  [optional] |
 
 
 
--- src/main/java/com/tencentcloudapi/im/model/ImSetAttrRequest.java
+++ src/main/java/com/tencentcloudapi/im/model/ImSetAttrRequest.java
@@ -8,8 +8,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.tencentcloudapi.im.model.ImGetAttrResponseAllOfUserAttrs;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import javax.validation.constraints.*;
@@ -20,73 +23,49 @@ import org.hibernate.validator.constraints.*;
  * ImSetAttrRequest
  */
 @JsonPropertyOrder({
-  ImSetAttrRequest.JSON_PROPERTY_TO_ACCOUNT,
-  ImSetAttrRequest.JSON_PROPERTY_ATTRS
+  ImSetAttrRequest.JSON_PROPERTY_USER_ATTRS
 })
 
 public class ImSetAttrRequest {
-  public static final String JSON_PROPERTY_TO_ACCOUNT = "To_Account";
-  private String toAccount;
-
-  public static final String JSON_PROPERTY_ATTRS = "Attrs";
-  private Object attrs;
+  public static final String JSON_PROPERTY_USER_ATTRS = "UserAttrs";
+  private List<ImGetAttrResponseAllOfUserAttrs> userAttrs = null;
 
   public ImSetAttrRequest() { 
   }
 
-  public ImSetAttrRequest toAccount(String toAccount) {
+  public ImSetAttrRequest userAttrs(List<ImGetAttrResponseAllOfUserAttrs> userAttrs) {
     
-    this.toAccount = toAccount;
+    this.userAttrs = userAttrs;
+    return this;
+  }
+
+  public ImSetAttrRequest addUserAttrsItem(ImGetAttrResponseAllOfUserAttrs userAttrsItem) {
+    if (this.userAttrs == null) {
+      this.userAttrs = new ArrayList<>();
+    }
+    this.userAttrs.add(userAttrsItem);
     return this;
   }
 
    /**
-   * 目标用户帐号列表
-   * @return toAccount
+   * Get userAttrs
+   * @return userAttrs
   **/
-  @javax.annotation.Nonnull
-  @NotNull
-  @ApiModelProperty(required = true, value = "目标用户帐号列表")
-  @JsonProperty(JSON_PROPERTY_TO_ACCOUNT)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  @javax.annotation.Nullable
+  @Valid
+  @ApiModelProperty(value = "")
+  @JsonProperty(JSON_PROPERTY_USER_ATTRS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
 
-  public String getToAccount() {
-    return toAccount;
+  public List<ImGetAttrResponseAllOfUserAttrs> getUserAttrs() {
+    return userAttrs;
   }
 
 
-  @JsonProperty(JSON_PROPERTY_TO_ACCOUNT)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
-  public void setToAccount(String toAccount) {
-    this.toAccount = toAccount;
-  }
-
-
-  public ImSetAttrRequest attrs(Object attrs) {
-    
-    this.attrs = attrs;
-    return this;
-  }
-
-   /**
-   * 属性集合。每个属性是一个键值对，键为属性名，值为该用户对应的属性值。用户属性值不能超过50字节
-   * @return attrs
-  **/
-  @javax.annotation.Nonnull
-  @NotNull
-  @ApiModelProperty(required = true, value = "属性集合。每个属性是一个键值对，键为属性名，值为该用户对应的属性值。用户属性值不能超过50字节")
-  @JsonProperty(JSON_PROPERTY_ATTRS)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
-
-  public Object getAttrs() {
-    return attrs;
-  }
-
-
-  @JsonProperty(JSON_PROPERTY_ATTRS)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
-  public void setAttrs(Object attrs) {
-    this.attrs = attrs;
+  @JsonProperty(JSON_PROPERTY_USER_ATTRS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public void setUserAttrs(List<ImGetAttrResponseAllOfUserAttrs> userAttrs) {
+    this.userAttrs = userAttrs;
   }
 
 
@@ -99,21 +78,19 @@ public class ImSetAttrRequest {
       return false;
     }
     ImSetAttrRequest imSetAttrRequest = (ImSetAttrRequest) o;
-    return Objects.equals(this.toAccount, imSetAttrRequest.toAccount) &&
-        Objects.equals(this.attrs, imSetAttrRequest.attrs);
+    return Objects.equals(this.userAttrs, imSetAttrRequest.userAttrs);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(toAccount, attrs);
+    return Objects.hash(userAttrs);
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class ImSetAttrRequest {\n");
-    sb.append("    toAccount: ").append(toIndentedString(toAccount)).append("\n");
-    sb.append("    attrs: ").append(toIndentedString(attrs)).append("\n");
+    sb.append("    userAttrs: ").append(toIndentedString(userAttrs)).append("\n");
     sb.append("}");
     return sb.toString();
   }
